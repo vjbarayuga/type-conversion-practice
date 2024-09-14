46aebcc46c379494a3173b2c0efe34e95a8b1ca4
vjdbarayuga <vjdbarayuga@gmail.com>
2024-09-14 23:06:46 +0800

    initial commmit

---
+++ app.js
@@ -0,0 +1,17 @@
+//CONVERT STRING TO NUMBER
+
+// let amount = '100';
+// amount = parseInt(amount);
+// console.log(amount);
+// console.log(typeof amount);
+
+//CONVERT NUMBER TO STRING
+// let money = 100;
+// money = money.toString();
+// console.log(money);
+// console.log(typeof money);
+
+let floatValue = '100.39';
+floatValue = parseFloat(floatValue);
+console.log(floatValue);
+console.log(typeof floatValue);
